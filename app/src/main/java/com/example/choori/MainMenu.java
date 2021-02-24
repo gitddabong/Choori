c8e4c3264b779b0210d7fb8c46f309e2c5275a03
gitddabong <wonjong0701@naver.com>
2021-02-24 19:33:23 +0900

    메인메뉴 브랜치 start

---
+++ app/src/main/java/com/example/choori/MainMenu.java
@@ -0,0 +1,28 @@
+package com.example.choori;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenu extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+
+
+
+    }
+
+
+
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
